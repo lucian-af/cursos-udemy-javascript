4f7d4d7b623a7ec4d9eb90900efdf858a1f0b8f5
lucian <lucian_af@hotmail.com>
2020-07-13 22:46:55 -0300

    007 - Seção 5: Javascript: Função - <= Aula 67

---
+++ funcoes/paramPadrao.js
@@ -0,0 +1,30 @@
+// Aul 67 - Parametros padrao
+
+function soma1(a, b, c) {
+    a = a || 1
+    b = b || 1
+    c = c || 1
+
+    return a + b + c
+}
+
+console.log(soma1(), soma1(3), soma1(1, 2, 3), soma1(0, 0, 0)) // valores padrão internos
+
+// estrategia 2,3,4 para gerar valor padrao
+function soma2(a, b, c) {
+    a = a != undefined ? a : 1    
+    // 1 in arguments, ele testa se existe valor na posição 1 do arguments
+    // (array inicia em 0, b seria posicao 1)
+    b = 1 in arguments ? b : 1
+    c = isNaN(c) ? 1 : c // mais segura, em termos de somar valores
+
+    return a + b + c
+}
+console.log(soma2(), soma2(3), soma2(1, 2, 3), soma2(0, 0, 0)) // valores padrão internos
+
+///  valor  padrao do es2015
+
+function soma3(a = 1, b = 1, c = 1) {
+    return a + b + c
+}
+console.log(soma3(), soma3(3), soma3(1, 2, 3), soma3(0, 0, 0)) // valores padrão internos
